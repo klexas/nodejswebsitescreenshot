9e26f41e9449e6132ecfad4efba7eb96682b4ce5
Adam Robins <tvwebco@gmail.com>
2014-06-21 02:15:16 +0100

    Delete src.js

--- src.js
+++
@@ -1,7 +0,0 @@
-var webshot = require('./lib/webshot');
-
-webshot('adamrobins.com', '/home/user/Desktop/imgloaderphp/img/img.png', function(err) {
-  if (err) return console.log(err);
-  console.log('OK');
-});
-
